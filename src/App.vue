08aaaab3e5d9a1e1dcf313e088ec2ea25c58ae95
Milu Franz <mfranz@Milus-MacBook-Pro.local>
2019-07-30 16:22:07 -0400

    Add title and label

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,27 @@
 <template>
   <div id="app">
-    Test
+    <form @submit.prevent="addTodo">
+      <h3>Vue TO-DO List</h3>
+      <label>Enter to-do:</label>
+      <input type="text" v-model="item" />
+      <button>Submit</button>
+    </form>
   </div>
 </template>
 
 <script>
 export default {
-  name: "app"
+  name: "app",
+  data() {
+    return {
+      item: ""
+    };
+  },
+  methods: {
+    addTodo() {
+      this.item = "";
+    }
+  }
 };
 </script>
 
@@ -17,5 +32,34 @@ body {
 }
 #app {
   text-align: center;
+}
+
+label {
+  padding: 1em;
+}
+
+input {
+  color: #696969;
+  font-size: 1.5em;
+  outline: 0;
+  background-color: #05f8af;
+  border-width: 0 0 2px;
+  border-color: black;
+  margin-right: 1em;
+  width: 300px;
+}
+
+button {
+  border-color: transparent;
+  color: white;
+  font-weight: bold;
+  padding: 0.8em;
+  background-color: black;
+  border-radius: 15px;
+  text-transform: uppercase;
+}
+
+button:hover {
+  color: #ff04bd;
 }
 </style>
